2ea2bc02352fdfc646a15942019d0d21ef39ca15
Kvolian <virotor.vik@gmail.com>
2021-12-12 22:03:24 +0300

    Добавьте файлы проекта.

---
+++ SLAY(MI)/Equation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAY_MI_
+{
+    class Equation
+    {
+        public List<double> Y { get; set; }
+        public List<List<double>> X { get; set; }
+        public double Presicion { get; set;} 
+
+        public List<double> Calculate()
+        {
+            List<double> result = new() { 0,0,0,0,0};
+            List<double> resultPrev = new();
+            do
+            {
+                resultPrev.Clear();
+                result.ForEach(x =>  resultPrev.Add(x));
+                result[0] = (Y[0] - X[0][1] * resultPrev[1])/X[0][0];
+                result[1] = (Y[1] - resultPrev[0] * X[1][0] - resultPrev[2] * X[1][2]) / X[1][1];
+                result[2] = (Y[2] - resultPrev[1] * X[2][0] - resultPrev[3] * X[2][2]) / X[2][1];
+                result[3] = (Y[3] - resultPrev[2] * X[3][0] - resultPrev[4] * X[3][2]) / X[3][1];
+                result[4] = (Y[4] - resultPrev[3] * X[4][0]) / X[4][1];
+            }while (!IsPrecision(result, resultPrev));
+
+            
+
+
+            return result;
+        }
+
+
+        private bool IsPrecision(List<double> values, List<double> valuesPref)
+        {
+            List<double> vs = values.Zip(valuesPref).ToList().Select(x => Math.Abs(Math.Abs(x.First - x.Second)-Presicion)).ToList();
+            
+            return vs.All(x => x.CompareTo(Presicion) <= 0);
+        }
+
+
+    }
+}
---
+++ SLAY(MI)/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SLAY_MI_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            List<List<double>> Value = new();
+            List<double> Y = new() { Double.Parse(Y1.Text), Double.Parse(Y2.Text) , Double.Parse(Y3.Text), Double.Parse(Y4.Text) , Double.Parse(Y5.Text)};
+            Value.Add(new() { Double.Parse(C0.Text), Double.Parse(B0.Text) });
+            Value.Add(new() {Double.Parse(A1.Text), Double.Parse(C1.Text), Double.Parse(B1.Text) });
+            Value.Add(new() {Double.Parse(A2.Text), Double.Parse(C2.Text), Double.Parse(B2.Text) });
+            Value.Add(new() {Double.Parse(A3.Text), Double.Parse(C3.Text), Double.Parse(B3.Text) });
+            Value.Add(new() {Double.Parse(A4.Text), Double.Parse(C4.Text) });
+
+            Equation equation = new Equation();
+            equation.Y = Y;
+            equation.X = Value;
+            equation.Presicion = Math.Pow(10, Double.Parse(textBox1.Text));
+            var l = equation.Calculate();
+            int counter = 1;
+            textBox.Clear();
+            l.ForEach(x => { textBox.Text += String.Format("X{1} = {0:F2}\n", x,counter); counter++; });
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
